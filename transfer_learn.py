b3d7524c16c586e5065d1e5f27c0f3a722bcd439
Tharun Sankar <tharun.sankar@gmail.com>
2017-11-06 11:16:30 -0500

    Config file + updates

--- transfer_learn.py
+++ transfer_learn.py
@@ -5,15 +5,22 @@ Data can be formatted as such using im2rec.py as released by MXNet
 Modeled off of code from https://mxnet.incubator.apache.org/how_to/finetune.html
 '''
 
-os.chdir('~/Deformable-ConvNets/rfcn')
-
 import mxnet as mx
 import os
+import sys
+os.chdir('/home/tharun/Deformable-ConvNets/rfcn')
+print os.getcwd()
+
+sys.path.append(os.path.dirname(os.path.expanduser('~/Deformable-ConvNets/rfcn/symbols')))
+sys.path.append(os.path.dirname(os.path.expanduser('~/Deformable-ConvNets/lib/utils')))
+sys.path.append(os.path.dirname(os.path.expanduser('~/Deformable-ConvNets/rfcn')))
+from rfcn.symbols import *
+from rfcn.config.config import config, update_config
 import cv2
-from config.config import config, update_config
 import numpy as np
+import pprint
 
-
+update_config('transfer_config.yaml')
 
 def get_iterators(batch_size, data_shape=(3, 224, 224)):
     train = mx.io.ImageRecordIter(
@@ -36,18 +43,18 @@ def get_iterators(batch_size, data_shape=(3, 224, 224)):
     return (train, val)
 
 pprint.pprint(config)
-config.symbol = 'resnet_v1_101_rfcn_dcn'
-sym_instance = eval(config.symbol + '.' + config.symbol)()
+symbol = 'resnet_v1_101_rfcn_dcn'
+sym_instance = eval(symbol + '.' + symbol)()
 sym = sym_instance.get_symbol(config, is_train=True)
 
-def get_fine_tune_model(symbol, arg_params, num_classes, layer_names=['cls_prob_reshape', 'rpn_cls_prob'):
+def get_fine_tune_model(symbol, arg_params, num_classes, layer_names=['cls_prob_reshape', 'rpn_cls_prob']):
     all_layers = symbol.get_internals()
     net1 = all_layers[layer_names[0]+'_output']
     net2 = all_layers[layer_names[1]+'_output']
     # net = mx.symbol.FullyConnected(data=net, num_hidden=num_classes, name='fc1')
     net1 = mx.symbol.SoftmaxOutput(data=net1, name='softmax_cls')
     net2 = mx.symbol.SoftmaxOutput(data=net2, name='softmax_rpn')
-    new_args = dict({k:arg_params[k] for k in arg_params if 'fc1' not in k})
+    new_args = dict({k:arg_params[k] for k in arg_params if 'softmax_cls' not in k and 'softmax_rpn' not in k})
     group = mx.sym.Group([net1, net2])
     return (group, new_args)
 
@@ -71,6 +78,7 @@ def fit(symbol, arg_params, aux_params, train, val, batch_size, num_gpus):
 arg_params, aux_params = load_param(cur_path + '/../model/' + ('rfcn_dcn_coco' if not args.rfcn_only else 'rfcn_coco'), 0, process=True)
 (new_sym, new_args) = get_fine_tune_model(sym, arg_params, num_classes)
 batch_size = 32
+num_gpus = 1
 (train, val) = get_iterators(batch_size)
 mod_score = fit(new_sym, new_args, aux_params, train, val, batch_size, num_gpus)
 print 'Training accuracy: ' + mod_score
