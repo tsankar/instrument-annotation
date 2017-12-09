1e9485fb2354899bfa88a5edc06d58c58f1e75e4
Tharun Sankar <tharun.sankar@gmail.com>
2017-12-09 17:37:58 -0500

    Data augmentation

--- download_imagenet.py
+++ download_imagenet.py
@@ -75,7 +75,7 @@ def move_images(PATH_PREF='/home/tharun/data', dest='/home/tharun/data/ILSVRC'):
 
 def create_imgsets():
     ROOT_DIR = '/home/tharun/data/ILSVRC/ImageSets/DET'
-    for d in ['val']: # 'train', 'val', 'test'
+    for d in ['train1']: # 'train', 'val', 'test'
         DATA_DIR = os.path.join('/home/tharun/data/ILSVRC/Annotations/DET/', d)
 
         f = open(os.path.join(ROOT_DIR, d + '.txt'), 'w')
@@ -126,3 +126,66 @@ def inspect_xmls():
         e = xml.etree.ElementTree.parse(os.path.join(ANNO_DIR, f)).getroot()
         for obj in e.findall('object'):
             print obj.find('name').text + '|'
+
+import imgaug as ia
+from imgaug import augmenters as iaa
+import cv2
+
+def augment():
+    ia.seed(1)
+
+    IMG_DIR = '/home/tharun/data/ILSVRC/Data/DET/train'
+    ANNO_DIR = '/home/tharun/data/ILSVRC/Annotations/DET/train'
+    im_save = '/home/tharun/data/ILSVRC/Data/DET/train1'
+    anno_save = '/home/tharun/data/ILSVRC/Annotations/DET/train1'
+
+    seq = iaa.Sequential([
+        iaa.CropAndPad(percent=(-0.25, 0.25))
+    ])
+
+    for f in os.listdir(ANNO_DIR):
+        name = f.split('.')[0]
+        anno_path = os.path.join(ANNO_DIR, f)
+        img_path = os.path.join(IMG_DIR, name + '.JPEG')
+
+        if not os.path.exists(img_path):
+            continue
+
+        # Save original image/BB in new dir
+        shutil.copy2(anno_path, anno_save)
+        shutil.copy2(img_path, im_save)
+
+        image = cv2.imread(img_path)
+
+        tree = xml.etree.ElementTree.parse(anno_path)
+        e = tree.getroot()
+        bbox = e.find('object').find('bndbox')
+        xmin = bbox.find('xmin')
+        ymin = bbox.find('ymin')
+        xmax = bbox.find('xmax')
+        ymax = bbox.find('ymax')
+        fname = e.find('filename')
+
+        bbs = ia.BoundingBoxesOnImage([
+            ia.BoundingBox(x1=int(xmin.text), y1=int(ymin.text), x2=int(xmax.text), y2=int(ymax.text))
+        ], shape=image.shape)
+
+        # Create and save 4 augmented versions of the image
+        for i in range(0, 4):
+            seq_det = seq.to_deterministic()
+
+            image_aug = seq_det.augment_images([image])[0]
+            bbs_aug = seq_det.augment_bounding_boxes([bbs])[0]
+            bbs_aug = bbs_aug.remove_out_of_image().cut_out_of_image()
+
+            new_name = name + '_aug' + str(i)
+            cv2.imwrite(os.path.join(im_save, new_name + '.JPEG'), image_aug)
+
+            fname.text = new_name
+            if bbs_aug.bounding_boxes:
+                boxes = bbs_aug.bounding_boxes[0]
+                xmin.text = str(boxes.x1)
+                ymin.text = str(boxes.y1)
+                xmax.text = str(boxes.x2)
+                ymax.text = str(boxes.y2)
+                tree.write(os.path.join(anno_save, new_name + '.xml'))
